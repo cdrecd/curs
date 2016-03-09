0049269d8a2d5ed779ec50a4c3aa1ea7ebfcfd50
cdrecd <a_chrdili@hotmail.com>
2016-03-10 00:12:19 +0200

    assigment

--- site/week4/js/script.js
+++ site/week4/js/script.js
@@ -1,10 +1,19 @@
-var x = function () {
-	console.log(a == b);
-};
-var a = "5";
-var b = 5;
-x(a, b);
+var sum = 0
+for (var i = 0; i < 100; i++) {
+	sum += i;
+}
 
-var string = "Hello";
-string += " World";
-console.log(string + "!");
+console.log("sum of 0 though 99 is: " + sum);
+
+var x = 10;
+if ( (null) || (console.log("Hello")) || x > 5 ) {
+  console.log("Hello");
+}
+
+var company = new Object();
+company.name = "Facebook";
+company.ceo = new Object();
+company.ceo.bitch = "esma";
+company.address = "fuckoffstreet";
+
+console.log(company);
