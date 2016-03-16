2eb7db39fe20581460bb5d937726ea9b15de2bdc
cdrecd <a_chrdili@hotmail.com>
2016-03-16 18:43:17 +0200

    js

--- site/Collen-JS/js/script.js
+++ site/Collen-JS/js/script.js
@@ -10,9 +10,59 @@ var firstName = welcomeMsg("hi") */
 	}*/
 
 // Date() is built-in function and shows date.
-function displayDate() {
+/*function displayDate() {
 	document.getElementById("demo").innerHTML = Date();
+}*/
+
+/*
+function messanger () {
+	document.getElementById("will").innerHTML = "Clicked First Button";
 }
+
+function messanger2 (){
+	document.getElementById("will").innerHTML = "Clicked Second Button";
+}
+*/
+
+function openit() {
+	x = document.getElementById("opclo");
+	x.style.display = "block";
+}
+
+function closeit() {
+	x = document.getElementById("opclo");
+	x.style.display = "none";
+}
+
+
+function closeMe(){
+      // Find the element
+   		x=document.getElementById("demo");
+       //Option 1: Change the style attribute directly
+   		//x.style.display="none";
+  
+      //Option 2: Change the class
+   		x.className="closed"; //looking in CSS classes
+	}
+
+function openMe(){
+      // Find the element
+   		x=document.getElementById("demo");
+      //Option 1: Change the style attribute directly
+   		 //x.style.display="block";
+      
+      //Option 2: Change the class
+   		x.className="open"; //looking in CSS classes
+	}
+
+// "this"
+// "this" attribute allows element to refer to itself and allows you to access an element's info. Every object in the DOM has automatically generated "this".
+
+
+function displayId (element) {
+	console.log(element.id);
+}
+
 
 
 
