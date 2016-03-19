beac8358541731c59bcaeec543c7a0c29be535ad
cdrecd <a_chrdili@hotmail.com>
2016-03-19 13:47:07 +0200

    wk3

---
+++ site/Collen-JS/wk3/js/script.js
@@ -0,0 +1,9 @@
+var x = [14, 434, 6, 32, 54, 65];
+
+//JavaScript arrays are objectd
+console.log(x.length);
+console.log(x.sort());
+x.push(43, 11, 13);
+console.log(x)
+k = x[x.length - 1]
+console.log(k)
