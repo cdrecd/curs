500a9ce327ba4a55b4c740aeb386fd8bc8006098
cdrecd <a_chrdili@hotmail.com>
2016-03-20 13:48:29 +0200

    js

--- site/Collen-JS/wk3/js/script.js
+++ site/Collen-JS/wk3/js/script.js
@@ -7,3 +7,24 @@ x.push(43, 11, 13);
 console.log(x)
 k = x[x.length - 1]
 console.log(k)
+
+
+var fruits = ["banana", "orange","apple", "mango"];
+function loadFruits() {
+	document.getElementById("fruits").innerHTML = fruits;
+}
+
+function myFunction() {
+	var fruit = prompt("What is yor favorit fruit?");
+	fruits[fruits.length] = fruit;
+	document.getElementById("fruits").innerHTML = fruits;
+}
+
+var massivi = [12, 33, 1, 4, 53, 66, 12];
+
+sum = 0;
+for (i = 0; i <  massivi.length; i++) {
+	sum += massivi[i];
+}
+
+document.write("sum is: " + sum + ", length is: " + massivi.length + ", averange: " + sum/massivi.length);
